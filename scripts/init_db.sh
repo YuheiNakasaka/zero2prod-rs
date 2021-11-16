4aef2360f46395a0c382b4cdfbe692b6cf2d83d8
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-11-16 17:14:33 +0900

    Add the db option to be able to use  as password

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -31,6 +31,7 @@ then
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
+    -e POSTGRES_HOST_AUTH_METHOD=password \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
